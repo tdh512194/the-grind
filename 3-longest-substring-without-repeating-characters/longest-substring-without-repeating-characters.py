c7a7db9e60937852770bb8605ec054835ba26d1a
Ta Duc Huy (ted) <tdh512194@gmail.com>
2026-04-07 18:11:36 +0930

    Time: 11 ms (77.01%) | Memory: 19.3 MB (46.93%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,11 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        last_seen_idx = {}
+        last_seen_idx={}
         result = 0
         i = j = 0
         for j,c in enumerate(s):
             if c in last_seen_idx:
-                i = max(i,last_seen_idx[c]+1)
+                i = max(i,last_seen_idx[c]+1) # we dont go backward, last_seen_idx[c]+1 could be from an earlier index (than i), we are sure that the current window is non-repeating, so we jump here to the max of i or next of last seen.
             result = max(result, j - i + 1)
             last_seen_idx[c] = j
         return result
