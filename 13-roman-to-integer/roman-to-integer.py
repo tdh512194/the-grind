66f2848b462a05ffa42c41c1e525d7f5ad6c82d5
Ta Duc Huy (ted) <tdh512194@gmail.com>
2026-04-09 23:34:42 +0930

    Time: 3 ms (80.64%) | Memory: 19.3 MB (70.13%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,36 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbol = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        i = 0
+        j = 0
+        result = 0
+        tmp = 0
+        while j < len(s):
+            cur = s[j]
+            if j + 1 == len(s):
+                result += symbol[cur]
+                break
+            nex = s[j+1]
+            if ( 
+                ((cur == 'I') and (nex in ('V', 'X')))
+                or ((cur == 'X') and (nex in ('L', 'C')))
+                or ((cur == 'C') and (nex in ('D', 'M')))
+            ):
+                result += symbol[nex] - symbol[cur]
+                j = j + 2
+            else:
+                result += symbol[cur]
+                j = j + 1
+        return result
+
+
+
+
