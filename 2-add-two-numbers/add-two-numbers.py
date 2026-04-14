707637d536abfa187917b0e7a020e322101b5e3d
Ta Duc Huy (ted) <tdh512194@gmail.com>
2026-04-14 10:36:54 +0930

    Time: 3 ms (65.60%) | Memory: 19.4 MB (52.18%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,16 +9,15 @@ class Solution:
         carry = 0
         curr = dummyhead # linkedlist patter with a reference head and moving pointer
         while (l1 is not None) or (l2 is not None) or (carry != 0):
-            a1 = l1.val if l1 else 0
-            a2 = l2.val if l2 else 0
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
 
-            to_add = a1 + a2 + carry
-            newnode = ListNode(to_add%10)
-            # dummyhead.val = to_add//10
+            to_add = l1val + l2val + carry
+            newnode = ListNode(to_add%10) # initialize value of new node
             carry = to_add//10
 
-            curr.next = newnode
-            curr = newnode
+            curr.next = newnode # link the new node to the list attached to current node
+            curr = newnode # move the pointer
 
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
